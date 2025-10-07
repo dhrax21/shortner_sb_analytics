e9c6154cfc306cdfb04660e9319e4739b1a8c6d8
Dheeraj Singh <dhrax21bc@gmail.com>
2025-10-07 11:27:27 +0530

    Initial commit - Added Spring Boot project

---
+++ src/main/java/com/url/shortner/UrlShortnerSbApplication.java
@@ -0,0 +1,18 @@
+package com.url.shortner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class UrlShortnerSbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UrlShortnerSbApplication.class, args);
+	}
+
+	public String hello(){
+		return " Hi this is backend";
+	}
+
+}
