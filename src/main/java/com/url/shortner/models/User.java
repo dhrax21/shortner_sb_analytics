0eea9ccd829223366d6b3ade7fb2b9a33179fe57
Dheeraj Singh <dhrax21bc@gmail.com>
2025-10-07 14:07:34 +0530

    spring-security-adds

--- src/main/java/com/url/shortner/models/User.java
+++ src/main/java/com/url/shortner/models/User.java
@@ -10,9 +10,12 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    private String email;
     private String username;
 
     private String password;
 
     private String role ="ROLE_USER";
+
 }
